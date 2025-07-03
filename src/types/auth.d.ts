ca70aa4bb8b2ea1a7f90e972ee027af5b7bae8f2
phoenixfusionx <jameslovett502@gmail.com>
2025-07-03 06:42:26 -0500

    chore: expand TypeScript types

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -1,6 +1,16 @@
+import { Request } from "express";
+
 export interface TokenPayload {
   id: string;
   email: string;
   role?: string;
   [key: string]: any;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: TokenPayload;
+    }
+  }
 }
