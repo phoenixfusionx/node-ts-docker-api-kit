0f109ba63c87678f8bf7d5257dd0c391e63b419c
phoenixfusionx <jameslovett502@gmail.com>
2025-07-03 06:51:20 -0500

    chore: enable CORS in application

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./docs/swagger";
 import authRoutes from "./rotues/auth.routes";
 import usersRoutes from "./rotues/users.routes";
 
 const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: process.env.BASE_URL || "http://localhost:5000",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  maxAge: 86400, // 24 hours
+};
+
+app.use(cors(corsOptions));
 
 // Parse JSON requests
 app.use(express.json());
